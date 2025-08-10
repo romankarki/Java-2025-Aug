4e2f4176a24de59906f699a400025dab4b637508
romankarki <romnkrki@gmail.com>
2025-08-10 06:50:47 -0500

    linked list reversal

--- app/app.java
+++ app/app.java
@@ -3,6 +3,7 @@ import selections.Selection;
 import linkedlist.LinkedList;
 
 public class app {
+
     public static void main (String[] args){
         
         LinkedList linkedList = new LinkedList(10);
@@ -14,7 +15,18 @@ public class app {
         linkedList.getTail();
         linkedList.getLength();
         linkedList.removeLast();
+        linkedList.prepend(5);
         linkedList.printList();
-
+        linkedList.get(2);
+        linkedList.set(2, 99);
+        linkedList.insert(2, 45);
+        System.out.println("After Insertion");
+        linkedList.printList();
+        linkedList.remove(3);
+        System.out.println("After Deletion");
+        linkedList.printList();
+        linkedList.reverse();
+        System.out.println("After Reversing");
+        linkedList.printList();
     }
 }
--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -71,6 +71,101 @@ public class LinkedList {
         return temp;
     }
 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode; 
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
 
+        length++;
 
+    }
+
+    public Node removeFirst() {
+        if(length == 0) return null;
+        Node temp = head;
+            
+        head = head.next;
+        temp.next = null;
+        length--;
+        if(length == 0) {
+            head = null;
+            tail = null;
+        }
+        
+        return temp;        
+    }
+
+    public Node get(int index) {
+       if(index < 0 || index >= length) return null;
+       Node temp = head;
+       for(int i = 0; i < index; i++) {
+            temp = temp.next;
+       }
+       return temp; 
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if(temp == null) return false;
+        temp.value = value;
+        return true;
+    }
+
+    public boolean insert(int index, int value) {
+        if(index < 0 || index > length) return false;
+        if(index == 0) {
+            prepend(value);
+            return true;
+        }
+        if(index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if(index < 0 || index >= length) return null;
+        if(index == 1) {
+          return removeFirst();
+        }
+        if(index == length - 1) {
+           return removeLast();
+        }
+
+        Node targetNode = get(index);
+        Node prevNode = get(index - 1);
+
+        prevNode.next = targetNode.next;
+        targetNode.next = null;
+        
+        length--;
+        return targetNode;
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.next;
+        Node before = null;
+
+        for(int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
 }
